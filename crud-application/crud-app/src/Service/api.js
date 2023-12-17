d13c57651e8ff44821ea7847545885979d4e7bd0
karlorz <karl.logon@gmail.com>
2023-12-17 17:41:34 +0800

    update use json server

--- crud-application/crud-app/src/Service/api.js
+++ crud-application/crud-app/src/Service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const usersUrl = 'http://localhost:8080';
+// const usersUrl = 'http://localhost:8080'; // server in java
+const usersUrl = 'http://localhost:3002'; // json server: npm run json-server
 
 
 export const getUsers = async (id) => {
@@ -13,13 +14,16 @@ export const getUsers = async (id) => {
 }
 
 export const addUser = async (user) => {
-    return await axios.post(`${usersUrl}/user`, user);
+    // return await axios.post(`${usersUrl}/user`, user);
+    return await axios.post(`${usersUrl}/users`, user);
 }
 
 export const deleteUser = async (id) => {
-    return await axios.delete(`${usersUrl}/user/${id}`);
+    // return await axios.delete(`${usersUrl}/user/${id}`);
+    return await axios.delete(`${usersUrl}/users/${id}`);
 }
 
 export const editUser = async (id, user) => {
-    return await axios.put(`${usersUrl}/user`, user)
+    // return await axios.put(`${usersUrl}/user`, user)
+    return await axios.put(`${usersUrl}/users/${id}`, user)
 }
